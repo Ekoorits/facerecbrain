2bcf312a95661cc85b2f797b1b405562b3c4ce29
Ekoorits <enrikoorits@gmail.com>
2022-02-18 12:40:38 +0200

    Added webhost urls

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3001/register', {
+    fetch('https://boiling-castle-56706.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
